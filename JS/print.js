a1b9327dfc8afee62606942e2d5e1916af118fba
Jaskirat Singh <jasstxm@gmail.com>
2025-08-18 15:49:39 +0530

    [JS] | console.log() Command

---
+++ JS/print.js
@@ -0,0 +1,12 @@
+// let cel=14
+// let fah=((9/5)*cel)+32
+// console.log(fah)
+
+// let first="Jaskirat"
+// let last="Singh"
+
+// console.log(first+" "+last)
+
+// let radius=19;
+// let area=3.14*radius*radius
+// console.log(area)
