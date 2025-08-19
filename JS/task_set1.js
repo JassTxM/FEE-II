618b2cfec1276fc498c628581df5c85414aafca9
Jaskirat Singh <jasstxm@gmail.com>
2025-08-19 14:31:57 +0530

    [JS] | Task Set 1

---
+++ JS/task_set1.js
@@ -0,0 +1,30 @@
+//Temperature Converter
+// let cel=14
+// let fah=((9/5)*cel)+32
+// console.log(fah)
+
+
+
+
+
+//Combine Strings
+// let first="Jaskirat"
+// let last="Singh"
+// console.log(first+" "+last)
+
+
+
+
+//Area of Circle
+// let radius=19;
+// let area=3.14*radius*radius
+// console.log(area)
+
+
+
+//Simple Interest Calculator
+// let amount = 10000;
+// let rate = 6.25;
+// let time = 5;
+// let interest = (amount*rate*time)/100;
+// console.log("The Interest for the Principal Amount of "+amount+" with rate "+rate+" for time of "+time+" is "+interest);
