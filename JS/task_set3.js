88fe1d751ba5cafcf04a0d0dc2411f849e1c22c9
Jaskirat Singh <jasstxm@gmail.com>
2025-08-21 15:46:14 +0530

    [JS] | Task Set 3

---
+++ JS/task_set3.js
@@ -0,0 +1,54 @@
+//Greet the user
+// function greetUser(name) {
+//     console.log("Hello, " + name);
+// }
+// greetUser("Jaskirat Singh"); 
+
+
+
+
+//Return first element of an array
+// function getFirstElement(arr) {
+//     return arr[0];
+// }
+// console.log(getFirstElement([10, 20, 30])); 
+
+
+
+
+//Count even numbers in an array
+// function countEvenNumbers(arr) {
+//     let count = 0;
+//     for (let num of arr) {
+//         if (num % 2 === 0) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(countEvenNumbers([1, 2, 3, 4, 6])); 
+
+
+
+
+//Filter names with more than 5 characters
+// function filterNames(names) {
+//     return names.filter(function(name) {
+//         return name.length > 5;
+//     });
+// }
+// console.log(filterNames(["Hirde", "Jass", "Jai", "Alex"]));
+
+
+
+
+// const std={
+//     title:"JS Guide",
+//     author:"John"
+// }
+
+// console.log(std.title);
+// console.log(std.author);
+
+// std.title = 'Book1';
+// console.log(std.title);
